8c7db2a0230523d9cd04557c42ba1993c859bde2
devjuice <timkatv057@gmail.com>
2025-09-10 16:49:37 +0200

    Срочное сохранение

--- main.py
+++ main.py
@@ -11,9 +11,19 @@ def openFile():
 def closeFile():
     print("close file (not)")
 
-ttk.Button(mainframe, text="Новый текстовый файл", command=newFile)
-ttk.Button(mainframe, text="Открыть текстовый файл", command=openFile)
-ttk.Button(mainframe, text="Закрыть редактируемый файл", command=closeFile)
+t = Text(root, width = 40, height = 5, wrap = "none")
+ys = ttk.Scrollbar(root, orient = 'vertical', command = t.yview)
+xs = ttk.Scrollbar(root, orient = 'horizontal', command = t.xview)
+t['yscrollcommand'] = ys.set
+t['xscrollcommand'] = xs.set
+t.insert('end', "Lorem ipsum...\n...\n...")
+t.grid(column = 0, row = 0, sticky = 'nwes')
+xs.grid(column = 0, row = 1, sticky = 'we')
+ys.grid(column = 1, row = 0, sticky = 'ns')
+root.grid_columnconfigure(0, weight = 1)
+root.grid_rowconfigure(0, weight = 1)
+
+root.bind(<Ctrl><O>, newFile)
 
 text = StringVar()
 #text_entry = ttk.Entry(mainframe, width=32, textvariable=text)
