59e4cfe705beb38691405bd532bf7ccb31d1bfef
devjuice <timkatv057@gmail.com>
2025-09-10 16:38:37 +0200

    Наработки в графическом пользовательском интерфейсе

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import tkinter
-tkinter._test()
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.title("Текстовый редактор 'Анаконда'")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+
+def newFile():
+    print("new file (not)")
+def openFile():
+    print("open file (not)")
+def closeFile():
+    print("close file (not)")
+
+ttk.Button(mainframe, text="Новый текстовый файл", command=newFile)
+ttk.Button(mainframe, text="Открыть текстовый файл", command=openFile)
+ttk.Button(mainframe, text="Закрыть редактируемый файл", command=closeFile)
+
+text = StringVar()
+#text_entry = ttk.Entry(mainframe, width=32, textvariable=text)
+#text_entry.grid(column=32, row=1, sticky=(W, E))
+#text_entry = ttk.
+
+root.mainloop()
+
+# начальный экран
+# на потом
